398bc3b234b5221039f2a8d6c9f7be37ecc9fba9
arch-spatula <shkim516@naver.com>
2024-01-23 22:24:49 +0900

    보드 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,122 @@
 <template>
-  <div>{{ snapshot.context.game }}</div>
-  <button @click="start">start</button>
-  <button @click="decision">decision</button>
-  <button @click="restart">restart</button>
+  <div>현재 게임 상태: {{ snapshot.context.game }}</div>
+  <div>
+    <button @click="start">start</button>
+    <button @click="decision">decision</button>
+    <button @click="restart">restart</button>
+  </div>
+  <div>현재 플레이어 턴: {{ snapshot.context.turn }}</div>
+  <div>
+    <button @click="toggle">toggle</button>
+    <div>{{ snapshot.context.activeIdx }}</div>
+  </div>
+  <div class="board">
+    <div>보드</div>
+    <ul class="outer-row">
+      <li v-for="(outerRow, outerRowIdx) in snapshot.context.square" :key="outerRowIdx">
+        <ul class="outer-col">
+          <li v-for="(outerCol, outerColIdx) in outerRow" :key="outerColIdx">
+            <ul class="inner-row">
+              <li v-for="(innerRow, innerRowIdx) in outerCol" :key="innerRowIdx">
+                <ul class="inner-col">
+                  <li v-for="(innerCol, innerColId) in innerRow" :key="innerColId">
+                    <button
+                      class="square-btn"
+                      @click="move(innerColId, innerRowIdx, outerColIdx, outerRowIdx)"
+                    >
+                      <p>{{ innerCol }}</p>
+                    </button>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { useCssModule } from 'vue'
 import { createMachine, assign } from 'xstate'
 import { useMachine } from '@xstate/vue'
 
+const INIT_SQUARE = 3
+
+const createEmptySquare = (size: number): Array<'O' | 'X' | 'empty'> => {
+  return Array.from({ length: size }, () => 'empty')
+}
+
 // 대기(wait) -시작-> 플레이(play) -판정-> 승부(result) -재시작-> 대기
 // O -> X -> O
-//
+// 시작할 때 칸수 정의
+// 칸수에 해당하는 중첩 배열 만들기
 const gameMachine = createMachine({
   id: 'game',
   types: {} as {
     context: {
       game: 'wait' | 'play' | 'result'
+      turn: 'O' | 'X'
+      square: ('O' | 'X' | 'empty')[][][][]
+      size: number
+      activeIdx: number
     }
-    events: { type: 'start' } | { type: 'decision' } | { type: 'restart' }
+    events:
+      | { type: 'start' }
+      | { type: 'decision' }
+      | { type: 'restart' }
+      | { type: 'toggle' }
+      | {
+          type: 'move'
+          value: {
+            innerColIdx: number
+            innerRowIdx: number
+            outerColIdx: number
+            outerRowIdx: number
+          }
+        }
   },
-  context: { game: 'wait' },
+  context: {
+    game: 'wait',
+    turn: 'O',
+    square: Array.from({ length: INIT_SQUARE }, () => [
+      ...Array.from({ length: INIT_SQUARE }, () => [
+        ...Array.from({ length: INIT_SQUARE }, () => createEmptySquare(INIT_SQUARE))
+      ])
+    ]),
+    size: 3,
+    activeIdx: -1
+  },
   on: {
     start: { actions: assign({ game: 'play' }) },
     decision: { actions: assign({ game: 'result' }) },
-    restart: { actions: assign({ game: 'wait' }) }
+    restart: { actions: assign({ game: 'wait', turn: 'O', square: [] }) },
+    toggle: {
+      actions: assign({
+        turn: ({ context }) => {
+          if (context.turn === 'O') return 'X'
+          else return 'O'
+        }
+      })
+    },
+    move: {
+      actions: assign({
+        turn: ({ context }) => {
+          if (context.turn === 'O') return 'X'
+          else return 'O'
+        },
+        activeIdx: ({ event }) => {
+          return event.value.innerColIdx + event.value.innerRowIdx * 3
+        },
+        square: ({ event, context }) => {
+          context.square[event.value.outerRowIdx][event.value.outerColIdx][event.value.innerRowIdx][
+            event.value.innerColIdx
+          ] = context.turn
+          return context.square
+        }
+      })
+    }
+    // setSize
   }
 })
 
@@ -42,68 +133,58 @@ function decision() {
 function restart() {
   send({ type: 'restart' })
 }
+
+function toggle() {
+  send({ type: 'toggle' })
+}
+
+function move(innerColId: number, innerRowIdx: number, outerColIdx: number, outerRowIdx: number) {
+  send({ type: 'move', value: { innerColIdx: innerColId, innerRowIdx, outerColIdx, outerRowIdx } })
+}
 </script>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style scoped lang="scss">
+.outer-col {
+  display: flex;
+  gap: 2rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.outer-row {
+  display: flex;
+  gap: 1.75rem;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.inner-col {
+  display: flex;
+  gap: 1rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.inner-row {
+  display: flex;
+  gap: 0.75rem;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+.square-btn {
+  all: unset;
+  width: 3.75rem;
+  height: 3.75rem;
+  background-color: #f9fafb;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0.5rem;
+  &:hover {
+    background-color: #f3f4f6;
+    cursor: pointer;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  &:active {
+    background-color: #e5e7eb;
   }
 }
 </style>
